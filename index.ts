a0fc2c7c191f83850e0e91472c4efacbf6762465
Amaan Coovadia <amaanc123@gmail.com>
2024-10-05 04:49:38 +0200

    Fixing the website

--- index.ts
+++ index.ts
@@ -5,6 +5,10 @@
 const reviewTotalDisplay = document.querySelector(
   "#reviews"
 ) as HTMLElement | null;
+const userNameDisplay = document.querySelector("#user") as HTMLElement | null;
+const returningUserDisplay = document.querySelector(
+  "#returning-user"
+) as HTMLElement | null;
 
 const reviews = [
   {
@@ -43,3 +47,26 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
+const you = {
+  userName: "Bobby",
+  isReturning: true,
+};
+
+function populateUser(isReturning: boolean, userName: string) {
+    if (returningUserDisplay) {
+        if (isReturning) {
+          returningUserDisplay.innerHTML = "back";
+        }
+      } else {
+        console.error("Returning user display element not found");
+      }
+    
+      if (userNameDisplay) {
+        userNameDisplay.innerHTML = userName;
+      } else {
+        console.error("Username display element not found");
+      }
+}
+    
+populateUser(you.isReturning, you.userName);
