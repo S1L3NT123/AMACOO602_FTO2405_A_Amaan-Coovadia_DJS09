6cedcee1e3c0d18bf17716627a0ef5e154c9640b
Amaan Coovadia <amaanc123@gmail.com>
2024-10-06 05:06:05 +0200

    New property

--- index.ts
+++ index.ts
@@ -1,39 +1,44 @@
-import { showReviewTotal, populateUser, showDetails, getTopTwoReviews } from './utils'
-import { Price, Country } from './types'
-import { Permissions , LoyaltyUser } from './enums'
-import  Review  from './interfaces'
+import {
+  showReviewTotal,
+  populateUser,
+  showDetails,
+  getTopTwoReviews,
+} from "./utils";
+import { Price, Country } from "./types";
+import { Permissions, LoyaltyUser } from "./enums";
+import Review from "./interfaces";
 const propertyContainer = document.querySelector(
   ".properties"
 ) as HTMLElement | null;
 const footer = document.querySelector(".footer") as HTMLElement | null;
-const reviewContainer = document.querySelector('.reviews')as HTMLElement | null;
-const container = document.querySelector('.container')as HTMLElement | null;
-const button = document.querySelector('button')as HTMLElement | null;
+const reviewContainer = document.querySelector(
+  ".reviews"
+) as HTMLElement | null;
+const container = document.querySelector(".container") as HTMLElement | null;
+const button = document.querySelector("button") as HTMLElement | null;
 
 let isLoggedIn: boolean;
 
 const reviews: Review[] = [
   {
-      name: 'Sheia',
-      stars: 5,
-      loyaltyUser: LoyaltyUser.GOLD_USER,
-      date: '01-04-2021'
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: LoyaltyUser.GOLD_USER,
+    date: "01-04-2021",
   },
   {
-      name: 'Andrzej',
-      stars: 3,
-      loyaltyUser: LoyaltyUser.BRONZE_USER,
-      date: '28-03-2021'
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: LoyaltyUser.BRONZE_USER,
+    date: "28-03-2021",
   },
   {
-      name: 'Omar',
-      stars: 4,
-      loyaltyUser: LoyaltyUser.SILVER_USER,
-      date: '27-03-2021',
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: LoyaltyUser.SILVER_USER,
+    date: "27-03-2021",
   },
-]
-;
-
+];
 // const you: {
 //     firstName: string;
 //     lastName: string;
@@ -57,112 +62,128 @@ const you = {
   stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
 
-const properties : {
+interface Property {
   image: string;
   title: string;
-  price: number;
+  price: Price;
   location: {
-      firstLine: string;
-      city: string;
-      code: number;
-      country: string;
+    firstLine: string;
+    city: string;
+    code: number | string;
+    country: Country;
   };
-  contact: [ number, string ];
+  contact: [number, string];
   isAvailable: boolean;
-}[] = [
+}
+
+const properties: Property[] = [
   {
-      image: 'images/colombia-property.jpg',
-      title: 'Colombian Shack',
-      price: 45,
-      location: {
-          firstLine: 'shack 37',
-          city: 'Bogota',
-          code: 45632,
-          country: 'Colombia'
-      },
-      contact: [+112343823978921, 'marywinkle@gmail.com'],
-      isAvailable: true  
+    image: "images/colombia-property.jpg",
+    title: "Colombian Shack",
+    price: 45,
+    location: {
+      firstLine: "shack 37",
+      city: "Bogota",
+      code: 45632,
+      country: "Colombia",
+    },
+    contact: [+112343823978921, "marywinkle@gmail.com"],
+    isAvailable: true,
   },
   {
-      image: 'images/poland-property.jpg',
-      title: 'Polish Cottage',
-      price: 34,
-      location: {
-          firstLine: 'no 23',
-          city: 'Gdansk',
-          code: 343903,
-          country: 'Poland'
-      },
-      contact: [+1298239028490830, 'garydavis@hotmail.com'],
-      isAvailable: false 
+    image: "images/poland-property.jpg",
+    title: "Polish Cottage",
+    price: 30,
+    location: {
+      firstLine: "no 23",
+      city: "Gdansk",
+      code: 343903,
+      country: "Poland",
+    },
+    contact: [+1298239028490830, "garydavis@hotmail.com"],
+    isAvailable: false,
   },
   {
-      image: 'images/london-property.jpg',
-      title: 'London Flat',
-      price: 23,
-      location: {
-          firstLine: 'flat 15',
-          city: 'London',
-          code: 35433,
-          country: 'United Kingdom',
-      },
-      contact: [+34829374892553, 'andyluger@aol.com'],
-      isAvailable: true
-  }
-]
+    image: "images/london-property.jpg",
+    title: "London Flat",
+    price: 25,
+    location: {
+      firstLine: "flat 15",
+      city: "London",
+      code: "SW4 5XW",
+      country: "United Kingdom",
+    },
+    contact: [+34829374892553, "andyluger@aol.com"],
+    isAvailable: true,
+  },
+  {
+    image: "images/malaysian-hotel.jpeg",
+    title: "Malia Hotel",
+    price: 35,
+    location: {
+      firstLine: "Room 4",
+      city: "Malia",
+      code: 45334,
+      country: "Malaysia",
+    },
+    contact: [+60349822083, "lee34@gmail.com"],
+    isAvailable: false,
+  },
+];
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+
 populateUser(you.isReturning, you.firstName);
-
-
 
 if (propertyContainer) {
   for (let i = 0; i < properties.length; i++) {
-    const card = document.createElement('div');
-    card.classList.add('card');
+    const card = document.createElement("div");
+    card.classList.add("card");
     card.innerHTML = properties[i].title;
-    const image = document.createElement('img');
-    image.setAttribute('src', properties[i].image);
+    const image = document.createElement("img");
+    image.setAttribute("src", properties[i].image);
     card.appendChild(image);
     showDetails(you.permissions, card, properties[i].price);
     propertyContainer.appendChild(card);
   }
-} 
+}
 
 if (reviewContainer) {
   showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
-} 
+}
 
 if (button && container) {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     addReviews(reviews);
   });
-} 
+}
 
-let count = 0
-function addReviews(array: {
-  name: string;
-  stars: number;
-  loyaltyUser: LoyaltyUser;
-  date: string;
-}[]): void {
+let count = 0;
+function addReviews(
+  array: {
+    name: string;
+    stars: number;
+    loyaltyUser: LoyaltyUser;
+    date: string;
+  }[]
+): void {
   let count = 0;
   if (reviewContainer && container && button) {
     if (!count) {
       count++;
       const topTwo = getTopTwoReviews(array);
       for (let i = 0; i < topTwo.length; i++) {
-        const card = document.createElement('div');
-        card.classList.add('review-card');
+        const card = document.createElement("div");
+        card.classList.add("review-card");
         card.innerHTML = `${topTwo[i].stars} stars from ${topTwo[i].name}`;
         reviewContainer.appendChild(card);
       }
-      container.removeChild(button); 
+      container.removeChild(button);
     }
-  } 
+  }
 }
 
-button.addEventListener('click', () => addReviews(reviews))
+button.addEventListener("click", () => addReviews(reviews));
 
 let currentLocation: [string, string, number] = ["London", "11:35", 17];
 if (footer) {
@@ -176,27 +197,32 @@ if (footer) {
 }
 
 class MainProperty {
-  src: string
-  title: string
-  reviews: Review[]
+  src: string;
+  title: string;
+  reviews: Review[];
   constructor(src: string, title: string, reviews: Review[]) {
-      this.src = src
-      this.title = title
-      this.reviews = reviews
+    this.src = src;
+    this.title = title;
+    this.reviews = reviews;
   }
 }
 
 let yourMainProperty = new MainProperty(
-  'images/italian-property.jpg', 
-  'Italian House',
-  [{
-      name: 'Olive',
+  "images/italian-property.jpg",
+  "Italian House",
+  [
+    {
+      name: "Olive",
       stars: 5,
       loyaltyUser: LoyaltyUser.GOLD_USER,
-      date: '12-04-2021'
-  }] )
+      date: "12-04-2021",
+    },
+  ]
+);
 
-const mainImageContainer = document.querySelector('.main-image')
-const image = document.createElement('img')
-image.setAttribute('src', yourMainProperty.src)
-mainImageContainer.appendChild(image)
+const mainImageContainer = document.querySelector(".main-image");
+const image = document.createElement("img");
+image.setAttribute("src", yourMainProperty.src);
+if (mainImageContainer) {
+  mainImageContainer.appendChild(image);
+}
