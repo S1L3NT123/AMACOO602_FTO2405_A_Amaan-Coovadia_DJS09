23881884b13f34951b09daa0e5017b906f1475cc
Amaan Coovadia <amaanc123@gmail.com>
2024-10-06 02:49:04 +0200

    Done Array Types mini-challenge

--- index.ts
+++ index.ts
@@ -12,26 +12,31 @@ const returningUserDisplay = document.querySelector(
 
 let isOpen : boolean
 
-const reviews = [
+const reviews : {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
+}[] = [
   {
-    name: "Sheia",
-    stars: 5,
-    loyaltyUser: true,
-    date: "01-04-2021",
+      name: 'Sheia',
+      stars: 5,
+      loyaltyUser: true,
+      date: '01-04-2021'
   },
   {
-    name: "Andrzej",
-    stars: 3,
-    loyaltyUser: false,
-    date: "28-03-2021",
+      name: 'Andrzej',
+      stars: 3,
+      loyaltyUser: false,
+      date: '28-03-2021'
   },
   {
-    name: "Omar",
-    stars: 4,
-    loyaltyUser: true,
-    date: "27-03-2021",
+      name: 'Omar',
+      stars: 4,
+      loyaltyUser: true,
+      date: '27-03-2021'
   },
-];
+]
 
 function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
   const iconDisplay = isLoyalty ? "⭐" : "";
